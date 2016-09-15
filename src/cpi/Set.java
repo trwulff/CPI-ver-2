851d20cf3c7912e6ecf301e8b2c71e33eca536b6
twulf <twulf@192.168.1.115>
2016-09-15 01:00:26 -0400

    Added javadoc code for cpi.Net

--- src/cpi/Set.java
+++ src/cpi/Set.java
@@ -1,10 +1,21 @@
 package cpi;
 
 import cpi.tools.SetBase;
-
+/**
+ * 
+ * @author twulf
+ *
+ * @param <Type>
+ */
 public class  Set <Type> extends SetBase <Type> {
-	public Set(String table,String name,Type Default){
-		super(table,name,Default,true);
+	/**
+	 * 
+	 * @param table
+	 * @param key
+	 * @param Default
+	 */
+	public Set(String table,String key,Type Default){
+		super(table,key,Default,true);
 	}
 
 }
--- src/cpi/tools/Constants.java
+++ src/cpi/tools/Constants.java
@@ -1,13 +1,37 @@
 package cpi.tools;
-
+/**
+ * 
+ * @author twulf
+ *
+ */
 public class Constants {
-
+/**
+ * 
+ */
 	public static String TITLE="Robot";
+/**
+ * 
+ */
 	public static final String HC_KEY="Hard code enabled";
+/**
+ * 
+ */
 	public static final String HC_TABLE="Hard code values";
+/**
+ * 
+ */
 	public static final String TABLE_AND_KEYS_SEPARATOR=":";
+/**
+ * 
+ */
 	public static final String TABLE_AND_KEYS_TABLE=TITLE+"Tools/Transfer";
+	/**
+	 * 
+	 */
 	public static final String TABLE_AND_KEYS_KEY="Tables and Keys list";
+	/**
+	 * 
+	 */
 	public static final String TRANSFER_STATE_KEY="Transfer State";
 	
 	
