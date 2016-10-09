8bba22fea49afa024b7c5ecbe6d93e6a2d7719d5
twulf <twulf@192.168.1.115>
2016-10-08 21:04:14 -0400

    HCconstants class is not functional as is. Cannot use networkTables for setting the hard code because ofnetwork delay.

--- src/cpi/tools/HCconstants.java
+++ src/cpi/tools/HCconstants.java
@@ -6,8 +6,8 @@ import cpi.tools.Constants;
 
 public class HCconstants {
 	public static final void set(){
-		Boolean D0=true;NetworkTable.getTable(Constants.TITLE+"/"+Constants.HC_TABLE+"/"+"Table3").putValue("Key1", D0);
-		Double[] D1={66.,65.,64.}; NetworkTable.getTable(Constants.TITLE+"/"+Constants.HC_TABLE+"/"+"Table").putValue("Name1", D1);
+//		Boolean D0=true;NetworkTable.getTable(Constants.TITLE+"/"+Constants.HC_TABLE+"/"+"Table3").putValue("Key1", D0);
+//		Double[] D1={66.,65.,64.}; NetworkTable.getTable(Constants.TITLE+"/"+Constants.HC_TABLE+"/"+"Table").putValue("Name1", D1);
 	}
 
 }
